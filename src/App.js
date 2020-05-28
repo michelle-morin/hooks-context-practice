b983fb8f19dbb2d316615b62e4c088b6c3f35371
Michelle Morin <morin2m@gmail.com>
2020-05-28 10:14:49 -0700

    import counter view

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Container } from 'semantic-ui-react';
+import CounterView from './views/counter-view';
 
-function App() {
+export default function App() {
   return (
-    <div>
-    </div>
+    <Container>
+      <h1>React Hooks Context Demo</h1>
+      <CounterView />
+    </Container>
   );
 }
-
-export default App;
